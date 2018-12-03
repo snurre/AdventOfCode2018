fb516af96808d738e382ebfbc9be4e5578b289d3
ruben-holen <ruben.holen@king.com>
2018-12-03 10:03:52 +0000

    day3

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AdventOfCode2018
+
+https://adventofcode.com/2018
---
+++ src/Day03.kt
@@ -0,0 +1,77 @@
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Day03 {
+    data class Point(val x: Int, val y: Int)
+    data class Claim(
+        val id: Int,
+        val location: Point,
+        val size: Point
+    )
+
+    private fun getClaims(): List<Claim> {
+        val rx = Regex("#([0-9]+) @ ([0-9]+),([0-9]+): ([0-9]+)x([0-9]+)")
+        return File(this.javaClass.getResource("03.txt").path).useLines {
+            it.map { s ->
+                val m = rx.matchEntire(s)!!
+                Claim(
+                    m.groupValues[1].toInt(),
+                    Point(m.groupValues[2].toInt(), m.groupValues[3].toInt()),
+                    Point(m.groupValues[4].toInt(), m.groupValues[5].toInt())
+                )
+            }.toList()
+        }
+    }
+
+    private fun getSquare(claims: Collection<Claim>): Array<Array<ArrayList<Claim>>> {
+        var width = 0
+        var height = 0
+        claims.forEach { c ->
+            val currWidth = c.location.x + c.size.x
+            val currHeight = c.location.y + c.location.y
+            if (currWidth > width) width = currWidth
+            if (currHeight > height) height = currHeight
+        }
+        val size = Math.max(width, height)
+        return Array(size) { Array(size) { arrayListOf<Claim>() } }
+    }
+
+    private fun getClaimedSquare(claims: Collection<Claim>): Array<Array<ArrayList<Claim>>> {
+        val square = getSquare(claims)
+        claims.forEach { c ->
+            for (x in c.location.x until c.location.x + c.size.x) {
+                for (y in c.location.y until (c.location.y + c.size.y)) {
+                    square[x][y].add(c)
+                }
+            }
+        }
+        return square
+    }
+
+    @Test
+    fun part1() {
+        val square = getClaimedSquare(getClaims())
+        var overlaps = 0
+        for (x in 0 until square.size) {
+            for (y in 0 until square[x].size) {
+                if (square[x][y].size >= 2) overlaps++
+            }
+        }
+        println(overlaps)
+    }
+
+    @Test
+    fun part2() {
+        val claims = getClaims()
+        val square = getClaimedSquare(claims)
+
+        println(claims.find { c ->
+            for (x in c.location.x until c.location.x + c.size.x) {
+                for (y in c.location.y until c.location.y + c.size.y) {
+                    if (square[x][y].size > 1) return@find false
+                }
+            }
+            true
+        })
+    }
+}
