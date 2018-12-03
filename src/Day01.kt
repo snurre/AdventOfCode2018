767f8d474aaf44e373196e321a3b37c2080f8aff
snurre <snurre@gmail.com>
2018-12-03 17:05:14 +0000

    Day1&2

---
+++ src/Day01.kt
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Day01 {
+    private fun getNumbers(): List<Int> {
+        return File(this.javaClass.getResource("01.txt").path).useLines { it.map(String::toInt).toList() }
+    }
+
+    @Test
+    fun part1() {
+        println(getNumbers().sum())
+    }
+
+    @Test
+    fun part2() {
+        val numbers = getNumbers()
+        var frequency = 0
+        val frequencies = hashSetOf<Int>()
+        while (true)
+            for (delta in numbers) {
+                frequency += delta
+                if (!frequencies.add(frequency)) {
+                    println(frequency)
+                    return
+                }
+            }
+    }
+}
---
+++ src/Day02.kt
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Day02 {
+    private fun getBoxIds(): List<String> {
+        return File(this.javaClass.getResource("03.txt").path).readLines()
+    }
+
+    @Test
+    fun part1() {
+        var two = 0
+        var three = 0
+        getBoxIds().forEach { boxId ->
+            val chars = hashMapOf<Char, Int>()
+            boxId.forEach { c -> chars[c] = (chars[c] ?: 0) + 1 }
+            var hasTwo = false
+            var hasThree = false
+            chars.values.forEach { count ->
+                when (count) {
+                    2 -> hasTwo = true
+                    3 -> hasThree = true
+                }
+            }
+            if (hasTwo) two++
+            if (hasThree) three++
+        }
+        println("$two * $three = ${two * three}")
+    }
+
+    @Test
+    fun part2() {
+        val boxIds = getBoxIds()
+        for (i in 0 until boxIds[0].length) {
+            boxIds.forEach { a ->
+                boxIds.forEach { b ->
+                    if (a != b) {
+                        val charsA = a.toCharArray()
+                        val charsB = b.toCharArray()
+                        var isMatch = true
+                        for (j in 0 until charsA.size) {
+                            if (j != i && charsA[j] != charsB[j]) {
+                                isMatch = false
+                                break
+                            }
+                        }
+                        if (isMatch) {
+                            println("$i\n$a\n$b\n${a.toCharArray().filterIndexed { index, _ -> index != i }.joinToString("")}")
+                            return
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
