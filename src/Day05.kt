bf6a48bbe0b54a7e98ca902271f9cbc702b734ac
snurre <snurre@gmail.com>
2018-12-05 10:55:38 +0000

    Day 5

---
+++ src/Day05.kt
@@ -0,0 +1,45 @@
+import org.junit.jupiter.api.Test
+import java.io.File
+
+class Day05 {
+    private val data = File(this.javaClass.getResource("05.txt").path).readText().trim()
+    private val units = ('a'..'z').let {
+        val s = hashSetOf<String>()
+        it.forEach { c ->
+            s.add("$c${c.toUpperCase()}")
+            s.add("${c.toUpperCase()}$c")
+        }
+        s
+    }
+
+    private fun react(s: String): String {
+        val sb = StringBuilder()
+        var i = -1
+        while (++i < s.length - 1) {
+            if (s.substring(i, i + 2) in units) i++ else sb.append(s[i])
+            if (i == s.length - 2) sb.append(s[i] + 1)
+        }
+        return sb.toString()
+    }
+
+    private fun solve(s: String): String {
+        var polymer = s
+        while (polymer != { val p = react(polymer); polymer = p;p }()) {}
+        return polymer
+    }
+
+    @Test
+    fun part1() {
+        println(solve(data).length)
+    }
+
+    @Test
+    fun part2() {
+        var shortest = data.length
+        for (c in 'a'..'z') {
+            val polymer = solve(data.replace(c.toString(), "", true))
+            if (polymer.length < shortest) shortest = polymer.length
+        }
+        println(shortest)
+    }
+}
